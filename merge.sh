6a8e4d79beba82e6e39b0cd276252e31d35c29b0
jiacheng <jiachengcui5@gmail.com>
2026-03-11 08:47:33 +0400

    update

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES="0" python merge_lora_weights_and_save_hf_model.py \
   --version="/data/ironman/jiacheng/final_Omni_Data/ck/SIDA-7B" \
-  --weight="/data/ironman/jiacheng/final_Omni_Data/runs/finetune_PIXAR-7B_tao0.1/pytorch_model.bin" \
-  --save_path="/data/ironman/jiacheng/final_Omni_Data/ck/finetune_PIXAR-7B_tao0.1"
+  --weight="/data/ironman/jiacheng/final_Omni_Data/runs/finetune_PIXAR-7B_ours_seg-only_sem0.1/pytorch_model.bin" \
+  --save_path="/data/ironman/jiacheng/final_Omni_Data/ck/finetune_PIXAR-7B_ours_seg-only_sem0.1"
